a591bc87b7cf95bd3914aee694ebe09c1e3b4436
SeonJungKim <ksj083895@gmail.com>
2021-05-16 17:44:54 +0500

    Initial Commit

---
+++ Shared/SwiftUI_EnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_EnvironmentObjectApp.swift
+//  Shared
+//
+//  Created by Seogun Kim on 2021/05/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_EnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
